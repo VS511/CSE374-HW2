0ba4eabff86c0c76d1715bd0359331d480c7e872
Vaibhav Singh <vaibhavs@klaatu.cs.washington.edu>
2021-10-19 14:06:46 -0700

    part-1 working

--- getcourses.sh
+++ getcourses.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-#URLextractor ($1, $2) {
-    if [ $# -ne 2 ]; then
-        echo “$0 Requires 2 arguments” >&2
-        exit 1
-    fi
 
-    if [ ! -f $1 ]; then
-        echo "File does not exist" <& 1
-        exit 1
-    fi
+if [ $# -ne 2 ]; then
+    echo “$0 Requires 2 arguments” >&2
+    exit 1
+fi
+
+if [ ! -f $1 ]; then
+    echo "File does not exist" <& 1
+    exit 1
+fi
 
 
-    grep -oE '(http|https)://(.*?)"' courses-index.html > $2
-    #grep -oe 'https?://[^ >]+' courses-index.html > $2
-    #grep -eo "https?://\S+?\.html" courses-index.html > output.txt
-    #sed -r 's/.*href="([^"]+).*/\1/g' courses-index.html ? $2
-    #sed 's/<p><a href=\([^>]*\).*/\1/' courses-index.html
-    #grep -o '<a href="http[^"]*"'|grep -v 'search?q=cache:'|grep -v '\.google\.'|sed 's/<a href="//;s/"$//' courses-index.html
-
-#}
+#grep -oE '(http|https)://(.*?)" ' courses-index.html > $2
+# (?:(?!X).)
+grep -o -E "https?://[][[:alnum:]._~:/?#@!$&v(*+,;%-]+" courses-index.html > $2.txt
