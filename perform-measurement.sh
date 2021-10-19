c3a8eec5970f68b8485272d424747550bf9dbd0b
DJMaltby <djtm@uw.edu>
2021-10-19 13:00:20 -0700

    first commit

---
+++ perform-measurement.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "$0 requires 1 argument"
+  exit 1
+fi
+
+if wget -q "$1" > /dev/null; then
+  wget -q -O - "$1" | wc -c
+  exit 0
+else
+  echo "0"
+  exit 1
+fi
+
+#if curl --output /dev/null --silent --head --fail "$1"; then
+ # wget -q -O - "$1" | wc -c
+ # exit 0
+#else
+ # echo "0"
+#fi
